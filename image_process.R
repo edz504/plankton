70ed4adcad84fdcbaa2791e471ce84dc165b2ce6
edz504 <eddiedzhou@gmail.com>
2014-12-16 16:24:13 -0800

    svm trained on the 9045, testing data preparing

--- image_process.R
+++ image_process.R
@@ -86,13 +86,91 @@ pred <- predict(svm_model, train.data.1[1:2000,],
     probability = TRUE)
 attr(pred, "probabilities")[1:10,]
 
-#
+# take the first N of each kind
+labels.df <- cbind(labels.df, seq(1, nrow(labels.df), by=1))
+colnames(labels.df) <- c("labels", "ind")
+
+# find the min and max index for each label (runs)
+inds.df <- labels.df %>% 
+    group_by(labels) %>%
+        summarise(first_ind=min(ind), last_ind=max(ind))
+
+# find pre-allocation space (first N)
+N <- 100
+prealloc <- 0
+for (i in seq(1, nrow(counts), by=1)) {
+    if (counts$count[i] > N) {
+        prealloc <- prealloc + N
+    } else {
+        prealloc <- prealloc + counts$count[i]
+    }
+}
+
+# pre-allocate (9045 here)
+selected.ind <- rep(NA, prealloc)
+j <- 1
+for (i in seq(1, nrow(inds.df), by=1)) {
+    if (inds.df$last_ind[i] - inds.df$first_ind[i] > N) {
+        these.inds <- seq(inds.df$first_ind[i],
+            inds.df$first_ind[i] + (N - 1), by=1)
+    } else {
+        these.inds <- seq(inds.df$first_ind[i],
+            inds.df$last_ind[i], by=1)
+    }
+
+    selected.ind[j: (j + length(these.inds) - 1)] <- these.inds
+    j <- j + length(these.inds)
+}
+
+# train svm on our selected data
+selected.train <- train.data.1[selected.ind,]
+selected.labels <- labels[selected.ind]
+
+start.time <- Sys.time()
+svm_select <- svm(selected.train, 
+    selected.labels, type='C', kernel='linear')
+end.time <- Sys.time()
+print(paste("Time taken for ", prealloc, " training samples = ",
+    (end.time - start.time), sep=""))
+# note: 12 minutes to train on 9045
+
+# check on the training sample
+start.time2 <- Sys.time()
+pred <- predict(svm_select, selected.train)
+end.time2 <- Sys.time()
+print(end.time2 - start.time2)
+# note: 3.5 min to predict on 9045 training
+
+# calculate training error
+err <- length(which(selected.labels != pred)) / length(pred)
+# 0 :)
+
+# prepare the testing set
+setwd("test")
+test.data.1 <- matrix(nrow = length(list.files()), 
+    ncol = 30 * 30)
+c <- 1
+i <- 1
+for (img in list.files()) {
+    if (i %% 10000 == 0) {
+        print(i)
+    }
+    test.data.1[i, ] <- as.vector(resize(readImage(img), 30, 30))
+    i <- i + 1
+}
+
+setwd(wd.top)
+save(test.data.1, file="testing_30x30.RData")
 
 
+# use on testing set given
+start.time3 <- Sys.time()
+pred <- predict(svm_select, test.data.1,
+    probability = TRUE)
+end.time3 <- Sys.time()
+print(end.time3 - start.time3)
 
-
-
-
+attr(pred, "probabilities")[1:10,]
 
 
 
