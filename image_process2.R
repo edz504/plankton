70933dab665d0a228b34fcfcef59ae415c207cce
edz504 <eddiedzhou@gmail.com>
2014-12-17 21:21:22 -0800

    added thresholding + segmentation for an additional feature

---
+++ image_process2.R
@@ -0,0 +1,84 @@
+# same as original image processing (30x30) but with
+# width-to-height ratio via segmentation as suggested
+library(EBImage) 
+library(e1071)
+
+# count the number of total images
+wd.top <- "C:/Users/edz504/Documents/Data Science Projects/Kaggle/plankton"
+wd.train <- paste(wd.top, "/train", sep="")
+setwd(wd.train)
+num.img <- 0
+for (class in list.files()) {
+    setwd(paste(wd.train, "/", class, sep=""))
+    num.img <- num.img + length(list.files())
+}
+
+
+# one extra feature, width-to-length ratio
+train.data.1 <- matrix(nrow = num.img, ncol = 1 + 30 * 30)
+labels <- rep(NA, num.img)
+setwd(wd.train)
+c <- 1
+i <- 1
+for (class in list.files()) {
+    setwd(paste(wd.train, "/", class, sep=""))
+    for (file in list.files()) {
+        img <- readImage(file)
+        # adaptive threshold
+        img2 <- thresh(img)
+        segment.labels <- bwlabel(img2)
+        # find largest non-background object
+        obj.label <- which.max(table(as.vector(segment.labels))[-1])
+        if (length(obj.label) == 0) {
+            ratio <- 0
+        } else {
+            obj.inds <- which(segment.labels == obj.label, 
+            arr.ind=TRUE)
+            ratio <- diff(range(obj.inds[,2])) / diff(range(obj.inds[,1]))
+        }
+        train.data.1[i, 1] <- ratio
+        train.data.1[i, 2:901] <- as.vector(resize(img, 30, 30))
+        labels[i] <- c
+        i <- i + 1
+    }
+    c <- c + 1
+    cat("Finished class '", class, "'\n", sep='')
+}
+
+# make labels a factor
+labels <- factor(labels)
+
+# save the training and testing data
+setwd(wd.top)
+save(train.data.1, labels, file="training_30x30_wrRatio.RData")
+
+
+# prepare the testing set
+setwd("test")
+test.data.1 <- matrix(nrow = length(list.files()), 
+    ncol = 1 + 30 * 30)
+c <- 1
+i <- 1
+for (img in list.files()) {
+    if (i %% 10000 == 0) {
+        cat(i, "\n")
+    }
+    # adaptive threshold
+    img2 <- thresh(img)
+    segment.labels <- bwlabel(img2)
+    # find largest non-background object
+    obj.label <- which.max(table(as.vector(segment.labels))[-1])
+    if (length(obj.label) == 0) {
+            ratio <- 0
+        } else {
+            obj.inds <- which(segment.labels == obj.label, 
+            arr.ind=TRUE)
+            ratio <- diff(range(obj.inds[,2])) / diff(range(obj.inds[,1]))
+        }
+    test.data.1[i, 1] <- ratio
+    test.data.1[i, 2:901] <- as.vector(resize(readImage(img), 30, 30))
+    i <- i + 1
+}
+
+setwd(wd.top)
+save(test.data.1, file="testing_30x30_wrRatio.RData")
---
+++ svm2_test.R
@@ -0,0 +1,49 @@
+library(e1071)
+library(dplyr)
+
+load("testing_30x30.RData")
+load("svmmodel_N2000_30x30.RData")
+top.wd <- "C:/Users/edz504/Documents/Data Science Projects/Kaggle/plankton/"
+
+setwd("test")
+test.files <- list.files()
+setwd(paste(top.wd, "train", sep=""))
+header <- t(data.frame(c("image", list.files())))
+
+# use on testing set given, in chunks of 1000 
+# init file first
+setwd(top.wd)
+write.table(header, file="submission3.csv", quote=FALSE,
+    col.names = FALSE, row.names=FALSE, sep=",")
+
+INCR <- 999
+i <- 1
+
+start.time2 <- Sys.time()
+while (TRUE) {
+    upper <- min(i + INCR, length(test.files))
+    these.inds <- i:upper
+
+    cat("Predicting testing samples ", 
+        min(these.inds), " to ", max(these.inds), "...\n", sep="")
+    pred <- predict(svm_select,
+        test.data.1[these.inds, ],
+        probability=TRUE)
+    probs <- attr(pred, "probabilities")
+    output <- cbind(test.files[these.inds], probs)
+    write.table(output, file="submission3.csv", append=TRUE,
+        quote=FALSE, col.names = FALSE, row.names=FALSE, sep=",")
+    if (length(these.inds) < 1000) {
+        break
+    }
+
+    i <- i + INCR + 1
+}
+end.time2 <- Sys.time()
+print(end.time2 - start.time2)
+# time to predict all with model trained on all: 2.04 hours
+
+
+      
+
+# resize upwards
