c8c3d292021bc15abe2714f70f66695d58f5faee
edz504 <eddiedzhou@gmail.com>
2014-12-19 14:27:58 -0800

    image processing in progress, svm 4 train and test written

--- image_process4.R
+++ image_process4.R
@@ -144,12 +144,12 @@ setwd(wd.top)
 save(train.data.scaled, labels, file="training_pixelandallEBfeat.RData")
 
 
-
+############################3
 
 # and testing data
 setwd("test")
 test.data.1 <- matrix(nrow = length(list.files()), 
-    ncol = 13)
+    ncol = 975)
 c <- 1
 i <- 1
 for (file in list.files()) {
@@ -174,34 +174,88 @@ for (file in list.files()) {
     test.data.1[i, 1] <- ratio
 
     ### EBImage Features
+
+    # shape features
     shape.feat <- computeFeatures.shape(segment.labels)
     if (max(segment.labels) == 0) {
+        max.area.ind <- 1
         test.data.1[i, 2:7] <- rep(NA, 6)
     } else {
-        test.data.1[i, 2:7] <- shape.feat[which.max(shape.feat[,1]),]
+        max.area.ind <- which.max(shape.feat[,1])
+        test.data.1[i, 2:7] <- shape.feat[max.area.ind,]
     }
 
+    # resized shape features
     img.resize <- resize(img, 30, 30)
     img.resize.thresh <- thresh(img.resize)
     resize.segment.labels <- bwlabel(img.resize.thresh)
     shape.feat.resize <- computeFeatures.shape(resize.segment.labels)
     if (max(resize.segment.labels) == 0) {
+        max.area.ind.resize <- 1
         test.data.1[i, 8:13] <- rep(NA, 6)
     } else {
-        test.data.1[i, 8:13] <- shape.feat.resize[which.max(shape.feat.resize[,1]),]
+        max.area.ind.resize <- which.max(shape.feat.resize[,1])
+        test.data.1[i, 8:13] <- shape.feat.resize[max.area.ind.resize,]
     }
+
+    # moment features
+    mom.feat <- computeFeatures.moment(segment.labels)
+    if (length(mom.feat) == 0) {
+        test.data.1[i, 14:18] <- rep(NA, 5)
+    } else {
+        test.data.1[i, 14:18] <- mom.feat[max.area.ind,]
+    }
+
+    # resized moment features
+    mom.feat.resize <- computeFeatures.moment(resize.segment.labels)
+    if (length(mom.feat.resize) == 0) {
+        test.data.1[i, 19:23] <- rep(NA, 5)
+    } else {
+        test.data.1[i, 19:23] <- mom.feat.resize[max.area.ind.resize,]
+    }
+
+    # haralick features
+    haral.feat <- tryCatch({
+        return(computeFeatures.haralick(img, segment.labels)[1,])
+    }, warning = function(w) {
+    }, error = function(e) {
+        return(rep(NA, 26))
+    }, finally = {
+    })
+    test.data.1[i, 24:49] <- haral.feat
+
+    # resized haralick features
+    haral.feat.resize <- tryCatch({
+        return(computeFeatures.haralick(img.resize,
+            resize.segment.labels)[1,])
+    }, warning = function(w) {
+    }, error = function(e) {
+        return(rep(NA, 26))
+    }, finally = {
+    })
+
+    test.data.1[i, 50:75] <- haral.feat.resize
+
+    # pixel features
+    test.data.1[i, 76:975] <- as.vector(img.resize)
+
     i <- i + 1
 }
 
-# fix NaNs, and Inf values
+# fix NaNs 
 test.data.1[which(is.nan(test.data.1), arr.ind=TRUE)] <- 0
 
+# fix Inf
 inf.inds <- which(test.data.1 == Inf, arr.ind=TRUE)
 test.data.1[inf.inds] <- -1
 this.max <- max(test.data.1[, 1])
 test.data.1[inf.inds] <- this.max
 
-# scale and save testing data
+# fix NA
+na.inds <- which(is.na(test.data.1), arr.ind=TRUE)
+test.data.1[na.inds] <- 0
+
+# scale and save the training data
 test.data.scaled <- scale(test.data.1)
 setwd(wd.top)
-save(test.data.scaled, file="testing_EBfeat.RData")
+save(test.data.scaled, labels, file="testing_pixelandallEBfeat.RData")
---
+++ svm4_train.R
@@ -0,0 +1,18 @@
+library(e1071)
+library(dplyr)
+
+wd.top <- "C:/Users/edz504/Documents/Data Science Projects/Kaggle/plankton"
+wd.train <- paste(wd.top, "/train", sep="")
+
+load("training_pixelandallEBfeat.RData")
+
+start.time <- Sys.time()
+svm_select <- svm(train.data.scaled,
+    labels, type='C', kernel='linear',
+    probability=TRUE)
+end.time <- Sys.time()
+print(end.time - start.time)
+# note: 12-18 minutes to train on 9045
+# note: 2.82 hours to train on 30336 (all)
+setwd(wd.top)
+save(svm_select, file="svmmodel_pixelandallEBfeat.RData")
